98c9e442205d8401c913400b7d7a571073ac3c5b
Toni March <amarch@brujula.es>
2019-05-14 10:12:11 +0200

    Se cambiaran valores en la bbdd

--- videoclub.sql
+++ videoclub.sql
@@ -7,25 +7,26 @@ USE videoclub;
 
 drop table if exists personas;
 create table personas(
-	id int not null auto_increment,
     dni varchar(20) not null,
     nombre varchar(100) not null,
     apellidos varchar(100) not null,
     sexo enum('M','F') not null,
     fechaNacimiento datetime not null,
     telefono varchar(10) not null,
-    primary key(id));
+    email varchar(255) not null,
+    primary key(dni));
     
 
 DROP TABLE IF EXISTS usuario;
 CREATE TABLE usuario(
-	id_persona INT NOT NULL,
+	id int not null auto_increment,
+	dni_persona varchar(20) NOT NULL,
     usuario varchar(50) NOT NULL,
     clave varchar(80) not null,
     tipo enum('A','O'),
     estado tinyint default 1,
-    primary key(id_persona),
-    foreign key(id_persona) references personas(id));
+    primary key(id),
+    foreign key(dni_persona) references personas(dni));
     
 drop table if exists generos;
 create table generos(
@@ -46,11 +47,11 @@ create table peliculas(
 drop table if exists alquileres;
 create table alquileres(
 	id_alquiler int not null auto_increment,
-    id_persona int not null,
+    id_usuario int not null,
     id_pelicula int not null,
     fechaAlquiler datetime not null,
     primary key(id_alquiler),
-    foreign key(id_persona) references personas(id),
+    foreign key(id_usuario) references usuario(id),
     foreign key(id_pelicula) references peliculas(id_pelicula));
     
     
