35270b072c6d436334f61d4cc3785b91a591b7e5
Toni March <amarch@brujula.es>
2019-05-17 13:14:24 +0200

    Se aplicara la logica para agregar peliculas

--- src/main/java/org/brujula/Controller/PeliculaController.java
+++ src/main/java/org/brujula/Controller/PeliculaController.java
@@ -1,14 +1,19 @@
 package org.brujula.Controller;
 
+import org.brujula.DAO.GeneroDAOImpl;
 import org.brujula.DAO.PeliculaDAOImpl;
+import org.brujula.DAO.util.GeneroDAO;
 import org.brujula.DAO.util.PeliculaDAO;
 import org.brujula.Model.Genero;
 import org.brujula.Model.Pelicula;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.List;
 
 @ManagedBean
 @ViewScoped
@@ -16,19 +21,54 @@ public class PeliculaController implements Serializable {
 
     private PeliculaDAO peliculaDAO;
 
+    private GeneroDAO generoDAO;
+
     private Pelicula pelicula;
 
     private Genero genero;
 
+    private List<Genero> listarGenero;
+
     @PostConstruct
     public void init(){
         peliculaDAO = new PeliculaDAOImpl();
+        generoDAO = new GeneroDAOImpl();
         pelicula = new Pelicula();
         genero = new Genero();
+        listarGenero = generoDAO.findAll();
+    }
+
+    public List<Genero> getListarGenero() {
+        return listarGenero;
+    }
+
+    public void setListarGenero(List<Genero> listarGenero) {
+        this.listarGenero = listarGenero;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
     }
 
     public void registrarPelicula(){
-        pelicula.setGenero(genero);
-        peliculaDAO.registrar(pelicula);
+        try{
+            pelicula.setGenero(genero);
+            peliculaDAO.registrar(pelicula);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Enhorabuena ", pelicula.getTitulo() + " se añadió a la lista"));
+        }catch (Exception e){ FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Aviso", "Error!"));}
+
     }
+
 }
